9ccb586c987e5e71877a08bb882c9fad54b752a9
qbh <qq@163.com>
2020-07-15 19:16:51 +0800

    修改工程day-0715-test01 bbb

--- src/main/java/com/qbh/git/test01.java
+++ src/main/java/com/qbh/git/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("Hello World");
+        System.out.println("bbbbb");
     }
 }
